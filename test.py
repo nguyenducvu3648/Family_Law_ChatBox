38974e6435e2cef5f5f95882cd230d735ecc9e31
ducvu <vu99550@donga.edu.vn>
2025-09-30 18:53:38 +0700

    Update .gitignore and remove replace.txt, test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyDa1OfiVFfBB9LOmLRVlL5tjR-hSkVHD3A")
-
-model = genai.GenerativeModel("gemini-2.5-flash")
-
-response = model.generate_content("Xin chào, bạn khỏe không?")
-print(response.text)
